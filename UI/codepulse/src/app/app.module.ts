990118f9eb2dbdeeea3a5f2c8e1cac6549afcdc6
waizin28 <waizinlinn.08@gmail.com>
2024-11-09 22:13:25 -0600

    modified UI

---
+++ UI/codepulse/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { CategoryListComponent } from './features/category/category-list/category-list.component';
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NavbarComponent,
+    CategoryListComponent,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ UI/codepulse/src/app/features/category/category-list/category-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-category-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './category-list.component.html',
+  styleUrls: ['./category-list.component.css'],
+})
+export class CategoryListComponent {}
