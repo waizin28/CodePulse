3e0cdddb05f090c9911d4279ed0e43da255743c4
waizin28 <waizinlinn.08@gmail.com>
2024-11-15 23:52:53 -0600

    get id from params at component

--- UI/codepulse/src/app/features/category/edit-category/edit-category.component.ts
+++ UI/codepulse/src/app/features/category/edit-category/edit-category.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-category',
   templateUrl: './edit-category.component.html',
-  styleUrls: ['./edit-category.component.css']
+  styleUrls: ['./edit-category.component.css'],
 })
-export class EditCategoryComponent {
+export class EditCategoryComponent implements OnInit {
+  id: string | null = null;
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    this.route.paramMap.subscribe({
+      next: (params) => {
+        // same name id defined at routing module
+        this.id = params.get('id');
+      },
+    });
+  }
 }
