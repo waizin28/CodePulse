853e7267e42e4ff83317ce1072fb77f9ff1bc099
waizin28 <waizinlinn.08@gmail.com>
2024-11-16 17:26:47 -0600

    created UI to edit category information

--- UI/codepulse/src/app/features/category/edit-category/edit-category.component.ts
+++ UI/codepulse/src/app/features/category/edit-category/edit-category.component.ts
@@ -1,21 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Category } from './../models/category.model';
+import { CategoryService } from './../services/category.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-edit-category',
   templateUrl: './edit-category.component.html',
   styleUrls: ['./edit-category.component.css'],
 })
-export class EditCategoryComponent implements OnInit {
+export class EditCategoryComponent implements OnInit, OnDestroy {
   id: string | null = null;
-  constructor(private route: ActivatedRoute) {}
+  paramsSubscription?: Subscription;
+  category?: Category;
+
+  constructor(
+    private route: ActivatedRoute,
+    private categoryService: CategoryService
+  ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe({
+    this.paramsSubscription = this.route.paramMap.subscribe({
       next: (params) => {
         // same name id defined at routing module
         this.id = params.get('id');
+        if (this.id) {
+          //get data from API for this category id
+          this.categoryService.getCategoryById(this.id).subscribe({
+            next: (response) => {
+              this.category = response;
+            },
+          });
+        }
       },
     });
+  }
+
+  ngOnDestroy(): void {
+    this.paramsSubscription?.unsubscribe();
   }
 }
