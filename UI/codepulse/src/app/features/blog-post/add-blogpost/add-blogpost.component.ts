71c04962803223164206d33ce21fbc579c6ac0ff
waizin28 <waizinlinn.08@gmail.com>
2024-11-17 18:25:32 -0600

    added form to create blog post

--- UI/codepulse/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
+++ UI/codepulse/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { AddBlogPost } from '../models/add-blog-post.model';
 
 @Component({
   selector: 'app-add-blogpost',
   templateUrl: './add-blogpost.component.html',
-  styleUrls: ['./add-blogpost.component.css']
+  styleUrls: ['./add-blogpost.component.css'],
 })
 export class AddBlogpostComponent {
+  addBlogModel: AddBlogPost;
 
+  constructor() {
+    this.addBlogModel = {
+      title: '',
+      shortDescription: '',
+      urlHandle: '',
+      content: '',
+      featuredImageUrl: '',
+      author: '',
+      isVisible: true,
+      publishedDate: new Date(),
+    };
+  }
+
+  onFormSubmit(): void {
+    console.log(this.addBlogModel);
+  }
 }
