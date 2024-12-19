a860fe1fb1812d3fb7489cdf6469419b215dd8be
waizin28 <waizinlinn.08@gmail.com>
2024-12-18 20:35:35 -0600

    added UI to get all blog posts

--- UI/codepulse/src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
+++ UI/codepulse/src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BlogPostService } from './../services/blog-post.service';
+import { Component, OnInit } from '@angular/core';
+import { BlogPost } from '../models/blog-post.model';
 
 @Component({
   selector: 'app-blogpost-list',
   templateUrl: './blogpost-list.component.html',
-  styleUrls: ['./blogpost-list.component.css']
+  styleUrls: ['./blogpost-list.component.css'],
 })
-export class BlogpostListComponent {
+export class BlogpostListComponent implements OnInit {
+  blogPosts$: Observable<BlogPost[]> | undefined;
 
+  constructor(private blogPostService: BlogPostService) {}
+
+  ngOnInit(): void {
+    // get all blogpost from api
+    // using async pipe
+    this.blogPosts$ = this.blogPostService.getAllBlogPosts();
+  }
 }
--- UI/codepulse/src/app/features/blog-post/services/blog-post.service.ts
+++ UI/codepulse/src/app/features/blog-post/services/blog-post.service.ts
@@ -17,4 +17,8 @@ export class BlogPostService {
       data
     );
   }
+
+  getAllBlogPosts(): Observable<BlogPost[]> {
+    return this.http.get<BlogPost[]>(`${environment.apiBaseUrl}/api/blogposts`);
+  }
 }
