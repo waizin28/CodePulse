108d4f183cf7db87816a200f2cf047a14cf61264
waizin28 <waizinlinn.08@gmail.com>
2024-12-20 16:37:31 -0600

    created 2 way data binding to edit category for a blog

--- UI/codepulse/src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
+++ UI/codepulse/src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { BlogPostService } from '../services/blog-post.service';
 import { BlogPost } from '../models/blog-post.model';
+import { CategoryService } from '../../category/services/category.service';
+import { Category } from '../../category/models/category.model';
 
 @Component({
   selector: 'app-edit-blogpost',
@@ -13,14 +15,19 @@ export class EditBlogpostComponent implements OnInit, OnDestroy {
   id: string | null = null;
   routeSubscription?: Subscription;
   blogPostModel?: BlogPost;
+  categories$?: Observable<Category[]>;
+  selectedCategories?: string[];
 
   constructor(
     private route: ActivatedRoute,
-    private blogPostService: BlogPostService
+    private blogPostService: BlogPostService,
+    private categoryService: CategoryService
   ) {}
 
   // get route param
   ngOnInit(): void {
+    this.categories$ = this.categoryService.getAllCategories();
+
     this.routeSubscription = this.route.paramMap.subscribe({
       next: (params) => {
         // same name id defined at routing module
@@ -31,6 +38,8 @@ export class EditBlogpostComponent implements OnInit, OnDestroy {
           this.blogPostService.getBlogPostById(this.id).subscribe({
             next: (response) => {
               this.blogPostModel = response;
+              // Get all the categories that are selected for this particular blog post
+              this.selectedCategories = response.categories.map((x) => x.id);
             },
           });
         }
