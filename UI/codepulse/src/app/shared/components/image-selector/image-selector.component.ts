ba155efb05e02a5d138093949888bf3d914b3343
waizin28 <waizinlinn.08@gmail.com>
2024-12-22 21:23:55 -0600

    added logic at ui to add images to blog

--- UI/codepulse/src/app/shared/components/image-selector/image-selector.component.ts
+++ UI/codepulse/src/app/shared/components/image-selector/image-selector.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { ImageService } from './image.service';
 
 @Component({
   selector: 'app-image-selector',
   templateUrl: './image-selector.component.html',
-  styleUrls: ['./image-selector.component.css']
+  styleUrls: ['./image-selector.component.css'],
 })
 export class ImageSelectorComponent {
+  private file?: File | null;
+  fileName: string = '';
+  title: string = '';
 
+  constructor(private imageService: ImageService) {}
+
+  onFileUploadChange(event: Event): void {
+    // get the file
+    const element = event?.currentTarget as HTMLInputElement;
+    this.file = element.files?.item(0);
+  }
+
+  uploadImage(): void {
+    if (this.file && this.fileName !== '' && this.title !== '') {
+      // Image service to upload image
+      this.imageService
+        .uploadImage(this.file, this.fileName, this.title)
+        .subscribe((response) => {
+          console.log(response);
+        });
+    }
+  }
 }
